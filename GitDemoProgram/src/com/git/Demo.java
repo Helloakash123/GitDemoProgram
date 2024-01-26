bac677198785feb9a1edb618315e420b50550a5c
akashchavhan123 <aschavhan999@gmail.com>
2024-01-26 23:50:41 +0530

    my first java program added

---
+++ GitDemoProgram/src/com/git/Demo.java
@@ -0,0 +1,15 @@
+package com.git;
+
+import java.util.ArrayList;
+
+public class Demo {
+	public static void main(String[] args) {
+		ArrayList<String> al = new ArrayList<String>();
+		al.add("pune");
+		al.add("mumbai");
+		al.add("bangalore");
+		al.add("delhi");
+		System.out.println(al);
+	}
+
+}
