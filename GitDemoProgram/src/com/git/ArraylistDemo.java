bc92b236a6fe7e39c81e0a72c8d98f0711257a64
Helloakash123 <dgtlnext@gmail.com>
2024-01-27 16:03:19 +0530

    arraylist program added

---
+++ GitDemoProgram/src/com/git/ArraylistDemo.java
@@ -0,0 +1,14 @@
+package com.git;
+
+import java.util.ArrayList;
+
+public class ArraylistDemo {
+	public static void main(String[] args) {
+		ArrayList al = new ArrayList<>();
+		al.add(12);
+		al.add(45);
+		al.add(78);
+		System.out.println(al);
+		}
+
+}
