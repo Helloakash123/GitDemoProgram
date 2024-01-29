a409679396b0b787b8c12b1225ad35a847bbe2eb
Helloakash123 <dgtlnext@gmail.com>
2024-01-29 12:46:51 +0530

    foreach added

--- GitPracticeDemo/src/com/git/practice/LinkdlistDemo.java
+++ GitPracticeDemo/src/com/git/practice/LinkdlistDemo.java
@@ -1,5 +1,15 @@
 package com.git.practice;
 
+import java.util.LinkedList;
+
 public class LinkdlistDemo {
+	
+	public static void main(String[] args) {
+		LinkedList<Integer> l = new LinkedList<>();
+		l.add(12);
+		l.add(2);
+		l.add(4);
+		System.out.println(l);
+	}
 
 }
